ee915a4eb5a7f02178a2d59c4b3613edf3a68f94
Vaibhav Kapoor <vaibhavkapoor.480@gmail.com>
2023-05-01 00:55:24 +0530

    Updates

---
+++ precache-manifest.ed53cb0a0e68eeb4be2f145ba242966b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535517782ddc627b1af7efd40a61c6b4",
+    "url": "/React-Movie-Search-App/index.html"
+  },
+  {
+    "revision": "f953dbe0713bf00ef806",
+    "url": "/React-Movie-Search-App/static/css/main.7aef1faa.chunk.css"
+  },
+  {
+    "revision": "b8214198425cc692b971",
+    "url": "/React-Movie-Search-App/static/js/2.238a241b.chunk.js"
+  },
+  {
+    "revision": "e8658e50dfaa215d03e17c686961427f",
+    "url": "/React-Movie-Search-App/static/js/2.238a241b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f953dbe0713bf00ef806",
+    "url": "/React-Movie-Search-App/static/js/main.bf916929.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/React-Movie-Search-App/static/js/main.bf916929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc445c059cc643e8f013",
+    "url": "/React-Movie-Search-App/static/js/runtime-main.cf885ab0.js"
+  }
+]);
